9000966327a185fe4f6f24adb6a66fa08c6f0a11
ed tsang <edwardt.mailmail.com>
2013-03-28 10:30:13 -0700

    adding unit test and the missing funcitonality implementation

--- src/ejabberd_auth_spark/src/spark_parse_loginData.erl
+++ src/ejabberd_auth_spark/src/spark_parse_loginData.erl
@@ -19,43 +19,57 @@
 -export[get_loginData/1,
         get_brandId_from_communityId/1].
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/file.hrl").
+-endif.
+
 %% @doc Retrieve login Data from Jid
 %%      returns the {brandid, integer}, {memberid, integer} or {error, Reason}
 %% @end
--spec get_isUserData(UserName::string())-> {error, atom()} | {ok, {atom(), integer},  {atom(), integer()}}.
+-spec get_loginData(UserName::string())-> {error, {brandid, atom()}, {memberid, atom()}, atom()}| {ok, {atom(), integer},  {atom(), integer()}}.
 get_loginData("") ->
   {error, user_missing}
 ;
 get_loginData(UserName) when (is_list(UserName)) ->
-  Community&MemberId = 
-		case re(UserName,"-") of 
- 		     [CommunityId, MemberId] -> [CommunityId, MemberId]
-                     {error, Reason} -> {error, Reason}
+  CommunityAndMemberId = 
+		case re:split(UserName,"-") of 
+ 		     [CommunityId, MemberId] -> [CommunityId, MemberId];
+                     {error, Reason} -> {error, Reason};
                      Else -> {error, Else}
   		end,
-  BrandId = case get_brandId_from_communityId(Community&MemberId) of
-                 {brandid, BrandId} -> BrandId,
-                 {error, Reason} -> {error, Reason}
-                 Else -> {error, Else}
-            end. 
+  case get_brandId_from_communityId(CommunityAndMemberId) of
+        {ok, {brandid, C}, {memberId, MemberId}} -> {ok, {brandid, C}, {memberId, MemberId}};
+        {error, {brandid, not_found}, {memberId, not_found}, Reason} -> {error, {brandid, not_found}, {memberId, not_found}, Reason};
+        Else -> {error, {brandid, not_found}, {memberId, not_found}, Else}
+  end,
+  RetValue;
+get_loginData(_) ->
+   {error, user_missing}. 
 
 %% 
 %% @doc Retrieve login Data from Jid
 %%      returns the {brandid, integer}, {memberid, integer} or {error, Reason}
 %% @end
-
-get_brandId_from_communityId(CommunityId) when (CommunityId > 0)->
+-spec get_loginData(UserName::string())-> {error, tuple(), term()} | {ok, {atom(), integer},  {atom(), integer()}}.
+get_brandId_from_communityId({error, Reason}) -> 
+  {error, {brandid, not_found}, {memberid, not_found}, Reason}; 
+get_brandId_from_communityId([CommunityId, MemberId]) when (CommunityId > 0)->
    Ids = case ejabberd_auth_spark:get_spark_auth_service_config({community2brandId,Host}) of
-             {error, Reason} -> {error, Reason},             
+             {error, Reason} -> {error, Reason};             
              Val -> find_Value(CommunityId, Ids)
          end,
    Val1 = case Ids of
-        {error, Reason} -> {error, Reason},
+        {error, Reason} -> {error, Reason};
         Val2 -> find_value(CommunityId, Ids)
    end,
    BrandId = extract_brandId(Val1),
-   BrandId.
-
+   case BrandId of
+        {ok, {brandid, C}} -> {ok, {brandid, C}, {memberId, MemberId}};
+        {error, Reason} -> {error, {brandid, not_found}, {memberId, not_found}, Reason}
+   end;
+get_brandId_from_communityId(_) -> 
+  {error, {brandid, not_found}, {memberid, not_found}, mal_formed}.
 
 %% @private
 %% @doc get community / brand id mapping from configure file
@@ -74,5 +88,36 @@ find_value(Key, List) ->
     end.
 
 extract_brandId({error, not_found}) -> {error, not_found};
-extract_brandId({_A,_B,C}) -> C;
+extract_brandId({_A,_B,C}) -> {ok, {brandid, C}};
 extract_brandId(_)-> {error, not_found}.
+
+
+%%%%%% EUNIT %%%%%%%%%%%%%%%%%%
+-ifdef(TEST).
+get_login_data_emptystring_test()-> 
+ 	?assertEqual({error, user_missing}, get_loginData("")).
+get_login_data_baddatatype_test()-> 
+ 	?assertEqual({error, user_missing}, get_loginData(wrongtype)).
+
+get_loginData_test() -> 
+        [?assertEqual([{ok, {brandid,}, {memberid, }}],get_loginData("12345-3")),
+         ?assertEqual({error, not_exist}, get_loginData("12345-99")),
+         ?assertEqual({error, not_exist}, get_loginData("12345-99")),
+         ?assertEqual({error, not_exist}, get_loginData("12345-99")),
+         ?assertEqual({error, not_exist}, get_loginData("12345-99")),
+        ].
+
+get_brandId_from_communityId_emptystring_test()-> 
+ 	?assertEqual({error, user_missing}, get_brandId_from_communityId("")).
+get_brandId_from_communityId_baddatatype_test()-> 
+ 	?assertEqual({error, user_missing}, get_brandId_from_communityId(wrongtype)).
+
+get_brandId_from_communityId_test() -> 
+        [?assertEqual([{ok, {brandid,}, {memberid, }}],get_brandId_from_communityId("12345-3")),
+         ?assertEqual({error, not_exist}, get_brandId_from_communityId("12345-99")),
+         ?assertEqual({error, not_exist}, get_brandId_from_communityId("12345-99")),
+         ?assertEqual({error, not_exist}, get_brandId_from_communityId("12345-99")),
+         ?assertEqual({error, not_exist}, get_brandId_from_communityId("12345-99")),
+        ].
+
+-endif.
