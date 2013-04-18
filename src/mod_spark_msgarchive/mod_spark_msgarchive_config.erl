bd54ff1f0b60b73f9b10c4c1accd9a73c3643336
root <root@localhost.localdomain>
2013-04-18 16:40:01 -0400

    message skeleton for config server

--- src/mod_spark_msgarchive/mod_spark_msgarchive_config.erl
+++ src/mod_spark_msgarchive/mod_spark_msgarchive_config.erl
@@ -1,4 +1,65 @@
+%%%----------------------------------------------------------------------
+%%%
+%%% @author : Edward Tsang <etsang@spark.net>
+%%% @doc Configuration file reader for mod_spark_msgarchive_config and saves 
+%%% 	 result in memory
+%%% Created : 18 Apr 2013
+%%%---------------------------------------------------------------------
+%%%
+%%% Copyright (c)
+%%%----------------------------------------------------------------------
+%%% @end
+
+%% @doc This 
+%%
+%%       internally it is using a rest client to authentication
+%% @end
 -module(mod_spark_msgarchive_config).
+-author('etsang@spark.net').
 -behaviour(gen_server).
 
--export().
+%% External exports
+%%
+-export([init/1, handle_cast/2, handle_info/2, terminate/1]).
+-export([handle_call/3, code_change/3]).
+
+%% @doc
+%%
+%% @end
+init(Args)->
+  .
+
+%% @doc
+%%
+%% @end
+handle_call()->
+ .
+
+
+%% @doc
+%%
+%% @end
+handle_cast() ->
+ .
+
+%% @doc
+%%
+%% @end
+handle_info() ->
+
+
+ .
+
+%% @doc
+%%
+%% @end
+terminate()->
+ .
+
+
+%% @doc
+%% 
+%% @end
+code_change()->
+ .
+
