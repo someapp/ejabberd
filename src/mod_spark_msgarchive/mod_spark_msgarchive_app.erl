c35b9ee8dac28a705c19d6906b7f39f6dcc249ce
root <root@localhost.localdomain>
2013-04-18 18:14:12 -0400

    determining the msgs for mod_spark_config

--- src/mod_spark_msgarchive/mod_spark_msgarchive_app.erl
+++ src/mod_spark_msgarchive/mod_spark_msgarchive_app.erl
@@ -1,6 +1,16 @@
+%% @author Edward Tsang <>
+%% @doc Top level application entry point for OTP
+%% @end
+
 -module(mod_spark_msgarhive_app).
+-author('etsang@spark.net').
 
 -behaviour(application).
+
+-include("etorrent_version.hrl").
+-include(""ejabberd.hrl).
+
+-define(APP, mod_spark_msgarchive).
 
 %% Application callbacks
 -export([start/2, stop/1]).
--- src/mod_spark_msgarchive/mod_spark_msgarchive_config.erl
+++ src/mod_spark_msgarchive/mod_spark_msgarchive_config.erl
@@ -18,10 +18,28 @@
 -author('etsang@spark.net').
 -behaviour(gen_server).
 
-%% External exports
+-define(DefaultMsgMaxSizeMb, 5).
+-define(DefaultThrottleIntervalSec, 5).
+-define(DefaultLogMaxSizeMb, 20).
+
+-include("ejabberd.hrl").
+-include("mod_spark_msgarchive.hrl").
+
+-define(?MODULE,SERVER).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/file.hrl").
+-endif.
+
+%%  gen_server callbacks
 %%
 -export([init/1, handle_cast/2, handle_info/2, terminate/1]).
 -export([handle_call/3, code_change/3]).
+
+%% External exports
+%% 
+-export([load_config/0, reload_config/0]). 
 
 %% @doc
 %%
@@ -30,10 +48,30 @@ init(Args)->
   .
 
 %% @doc
-%%
+%% Get the spark rest endpoint of sending missed IM 
 %% @end
-handle_call()->
+handle_call(send_missedIM_endpoint, )->
  .
+
+
+%% @doc
+%% Get the message queue endpoint of published missed IM to
+%% @end
+handle_call(rabbimq_endpoint, )->
+ .
+
+%% @doc
+%% Get the max size of messages to send/push
+%% @end
+handle_call(message_throttle_size, )->
+ .
+
+%% @doc
+%% Get the log files directory
+%% @end
+handle_call(mod_spark_achive_log_dir, )->
+ .
+
 
 
 %% @doc
@@ -63,3 +101,41 @@ terminate()->
 code_change()->
  .
 
+
+%%%%%%%%%%%% Private %%%%%%%%%%%%%%
+
+%% @private
+%%
+%% 
+
+%% @private
+%%
+%%
+
+%% @private
+%%
+%%
+
+%% @private
+%%
+%%
+
+%% @private
+%%
+%%
+
+
+%% @private
+%%
+%%
+
+
+
+
+
+%%%%%% EUNIT %%%%%%%%%%%%%%%%%%
+-ifdef(TEST).
+
+
+
+-endif.
