614e2c8482dd738013f3274560acfa86fb90bab5
root <root@localhost.localdomain>
2013-05-02 13:24:13 -0400

    adding restresponse check

--- src/mod_spark_msgarchive/spark_msgarchive_restclient.erl
+++ src/mod_spark_msgarchive/spark_msgarchive_restclient.erl
@@ -22,11 +22,13 @@
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
-
+%http://api.stgv3.spark.net/v2/brandId/1003/instantmessenger/missedIM?access_token=1/DNezb4VlVSRIyJ9JKrY36Df1a/S/+buAaZhRB51gzv4=&RecipientMemberId=1234&Messages=First
 -define(SERVER, ?MODULE).
--record(sendMissedIM, 
+-record(sendMissedIMMsg, 
 	{	
-	   		
+	   access_token
+           RecipientMemberId=
+           Messages  		
 	}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
