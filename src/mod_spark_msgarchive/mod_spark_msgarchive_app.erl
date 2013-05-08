f349652d40075d7f08497898a1667d46814553c7
root <root@localhost.localdomain>
2013-05-07 20:20:59 -0400

    adding supervisor code

--- src/mod_spark_msgarchive/mod_spark_msgarchive_app.erl
+++ src/mod_spark_msgarchive/mod_spark_msgarchive_app.erl
@@ -50,13 +50,15 @@ start(_StartType, _StartArgs) ->
      ApiConf = get_service_api_config(),
      RestCConf = get_restClient_config(),
      RabbitmqCConf = get_rabbitMQclient_config(),
+     case mod_spark_msgarhive_sup:start_link() of 
+	{ok, alread_started} -> ok;
+	ok -> ok;
+	{error, Reason} -> {error, Reason}
+	Else -> {error, Else}
+     end.
 
-
-    mod_spark_msgarhive_sup:start_link().
-
-stop(_State) ->
+stop(_State) ->   
     ok.
-
 
 
 %% ===================================================================
@@ -96,67 +98,12 @@ get_rabbitMQclient_config()->
 
 
 
-%% @doc
-%% A list of configuration specification that lists out which keys are 
-%% require, and which are optional
-%% @end
--spec configuration_spec()-> [configKV()].
-configuration_spec() ->
-   [ required(spark_auth_endpoint),
-     required(send_missed_im),
-     required(profile_memberstatus),
-     required(rabbitmq_endpoint),
-     optional(spark_oauth_access_token),
-     optional(rest_client_timeout_in_sec),
-     optional(rest_call_retry_attempt)
-   ].
-
-
-%% @doc
-%% Returns the final configuration list for application execution  
-%% @end
--spec load_config([]
-load_config([])->
-  ok,
-
-load_config([|])->
-  load_config
-  .
-
-
-reload() ->
-
- .
-
-
-check_config() ->
- 
-
- .
-
-%% @doc If the config key has no value, put in a default one
-%% @end
-optional(Key, Default)->
-
-%% @doc If the config key value is missing, it is error
-%% @end
-required(Key)->
-
-.
 
 
 
 %%%%%% EUNIT %%%%%%%%%%%%%%%%%%
 -ifdef(TEST).
 
-
-
-
-optionalKey_missing_value_test()->
-				.
-
-requiredKey_missing_value_test()->
-				.
 
 -endif.
 
